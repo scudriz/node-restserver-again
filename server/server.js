487814cf3bc63dd472bae4ff366070b97078c39f
scudriz <samue0109@gmail.com>
2019-10-03 21:25:35 -0500

    Deshago cambios

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get('/usuarios', function(req, res) {
-    res.json('getUsuarios Prueba');
+    res.json('getUsuarios');
 })
 
 app.post('/usuarios', function(req, res) {
