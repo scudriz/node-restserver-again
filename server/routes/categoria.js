659affba93e84f6e3489ccbc309e31ea52e738ba
scudriz <samue0109@gmail.com>
2019-10-13 21:51:39 -0500

    Final prueba SEED

--- server/routes/categoria.js
+++ server/routes/categoria.js
@@ -5,10 +5,6 @@ const app = express();
 
 app.get('/categorias', verificaToken, (req, res) => {
 
-    return res.json({
-        seed: process.env.SEED
-    })
-
     let desde = Number(req.query.desde) || 0;
     let limite = Number(req.query.limite) || 5;
 
